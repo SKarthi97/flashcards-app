affdec444cb794702458ac318b3eff9f440f2edb
karthick sivakumar <karthicksivakumar97@gmail.com>
2024-07-10 18:52:58 +0530

    connect the SQLite Database

--- flashcards_app/cards/models.py
+++ flashcards_app/cards/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+NUM_BOXES = 5
+BOXES = range(1, NUM_BOXES + 1)
+
+# word-to-word translations in the flashcards app
+class Card(models.Model):
+
+    question = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
+    # keep track of the box number where the card sits.
+    box = models.IntegerField(
+        choices=zip(BOXES, BOXES), # with choices, make sure that the integer must contain a number that's within the boxes range.
+        default=BOXES[0],
+    )
+    date_created = models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return self.question
+
