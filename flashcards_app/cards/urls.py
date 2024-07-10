84d793224a00988215eccba00f49e971e7cb8649
karthick sivakumar <karthicksivakumar97@gmail.com>
2024-07-10 17:28:03 +0530

    Corrected the template not found error

--- flashcards_app/cards/urls.py
+++ flashcards_app/cards/urls.py
@@ -6,7 +6,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path(
         "",
-        TemplateView.as_view(template_name="base.html"),
+        TemplateView.as_view(template_name="cards/base.html"),
         name="home"
     ),
 ]
