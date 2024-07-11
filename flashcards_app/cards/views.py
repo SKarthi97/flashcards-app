7a5518cfe5ff9276536da061f0202967e395f84f
karthick sivakumar <karthicksivakumar97@gmail.com>
2024-07-11 16:47:05 +0530

    Included a update view to update the existing card through a form

--- flashcards_app/cards/views.py
+++ flashcards_app/cards/views.py
@@ -1,6 +1,8 @@
 
+from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
+    UpdateView,
 )
 
 
@@ -9,3 +11,9 @@ from .models import Card
 class CardListView(ListView):
     model = Card
     queryset = Card.objects.all().order_by("box", "-date_created")
+
+
+class CardUpdateView(UpdateView):
+    model = Card
+    fields = ["question", "answer", "box"]
+    success_url = reverse_lazy("card-list")
