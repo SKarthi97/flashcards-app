b2147216ed913c169f3544d05a57aff4e97aa13b
karthick sivakumar <karthicksivakumar97@gmail.com>
2024-07-16 10:04:28 +0530

    Included the Inclusion tag function

--- flashcards_app/cards/templatetags/cards_tags.py
+++ flashcards_app/cards/templatetags/cards_tags.py
@@ -1,19 +1,21 @@
 # cards/templatetags/cards_tags.py
 
 from django import template
-
-from cards.models import BOXES, Card
+from django.shortcuts import get_object_or_404
+from cards.models import BOXES, Card, FlashCardSetList
 
 register = template.Library()
 
 @register.inclusion_tag("cards/box_links.html")
-def boxes_as_links():
+def boxes_as_links(set_id):
     boxes = []
+    flashcard_set = get_object_or_404(FlashCardSetList, id=set_id)
     for box_num in BOXES:
-        card_count =  Card.objects.filter(box=box_num).count()
+        card_count =  Card.objects.filter(flashcard_set=flashcard_set, box=box_num).count()
         boxes.append({
             "number": box_num,
             "card_count": card_count,
+            "set_id": set_id,
         })
         
     return {"boxes": boxes}
